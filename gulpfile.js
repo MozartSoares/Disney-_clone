e187bf0cdb1ecbfda252b51f1fe435d8a4d19394
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-05 21:52:02 -0300

    -Criado Hero -Importado fonte externa

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -8,11 +8,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css'))
 }
 
-function watch() {
-
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
