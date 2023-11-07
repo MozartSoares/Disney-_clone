83bd2837a9b15ad61b8eff726e1b26ffa00e30e0
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-07 19:27:04 -0300

    termino das seções e ajustes de responsividade

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const tabsContainer = document.querySelectorAll('[data-tab-id]');
-        
+    const questions = document.querySelectorAll('[data-faq-question]');
+    
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    //comportamento do cabeçalho
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual < alturaHero) {
+            ocultaHeader()
+        } else {
+            exibeHeader()
+        }
+    })
+
+    //comportamento de abas da seção shows
     for(let i = 0 ; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton
@@ -13,7 +28,29 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is--active')
         })
     }
+    //comportamento dos accordions FAQ
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+
 })
+
+function exibeHeader() {
+    const header = document.querySelector('header')
+    header.classList.remove('header--is--hidden')
+}
+
+function ocultaHeader() {
+    const header = document.querySelector('header')
+    header.classList.add('header--is--hidden')
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is--open'
+    const elementoPai = elemento.target.parentNode
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
